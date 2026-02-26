675354fe6f4b328508961005bd83bc4d6488d80a
Nyeso Edvard <edvardnyeso@Edvards-Macbook-Air.local>
2026-02-26 09:57:29 +0100

    chore: upgrade model from o3-mini to o3

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ module.exports = {
 
   // OpenAI
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  AI_MODEL: 'o3-mini',
+  AI_MODEL: 'o3',
 
   // Paths
   SANDBOX_ROOT: path.resolve(__dirname, '..', 'workspace'),
